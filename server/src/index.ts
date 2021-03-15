129afa5593c76ca4c89ea6d0c63138ecec73282f
Witold Zawada <witekzawada@o2.pl>
2021-03-15 20:01:05 +0100

    apollo server setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,36 @@
+import 'reflect-metadata'
 import { MikroORM } from '@mikro-orm/core' //ctrl + space for autocompletion
 import { __prod__ } from './constants'
-import { Post } from './entities/Post'
 import microConfig from './mikro-orm.config'
+import express from 'express'
+import { ApolloServer } from 'apollo-server-express'
+import { buildSchema } from 'type-graphql'
+import { HelloResolver } from './resolvers/hello'
+import { PostResolver } from './resolvers/post'
 
 const main = async () => {
     const orm = await MikroORM.init(microConfig)
     await orm.getMigrator().up()
-    // const post = orm.em.create(Post, {title: 'post title'}) // === new Post('post title')
-    // await orm.em.persistAndFlush(post)
 
-    // const posts = await orm.em.find(Post, {});
-    // console.log(posts);
+    const app = express()
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [
+                HelloResolver,
+                PostResolver
+            ],
+            validate: false
+        }),
+        context: () => ({ em: orm.em }) //special object that is accesibble from all your resolvers
+    })
+
+    apolloServer.applyMiddleware({
+        app
+    })
+
+    app.listen(4000, () => {
+        console.log(`Server started listening at port 4000. GLHF!`)
+    })
 }
 
 main().catch(err => {
