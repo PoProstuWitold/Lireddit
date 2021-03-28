c4bcf7112af8096b1fcae436cf3a521f21b4e8da
Witold Zawada <witekzawada@o2.pl>
2021-03-28 23:54:59 +0200

    basic frontend pagination

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -4,13 +4,20 @@ import { usePostsQuery } from '../generated/graphql'
 import { Layout } from '../components/Layout'
 import NextLink from 'next/link'
 import { Box, Button, Flex, Heading, Link, Stack, Text } from '@chakra-ui/react'
+import { useState } from 'react'
 
 const Index = () => {
 
+    const [variables, setVariables] = useState({
+       limit: 10,
+       cursor: null as null | string 
+    })
+
+    console.log('gowno', variables);
+    
+
     const [{ data, fetching }] = usePostsQuery({
-      variables: {
-        limit: 10
-      }
+      variables
     })
 
     if (!fetching && !data) {
@@ -41,7 +48,12 @@ const Index = () => {
       )}
       {data ? (
         <Flex>
-        <Button isLoading={fetching} m="auto" my={8}>
+        <Button onClick={() => {
+          setVariables({
+            limit: variables.limit,
+            cursor: data.posts[data.posts.length - 1].createdAt
+          })
+        }} isLoading={fetching} m="auto" my={8}>
           load more
         </Button>
       </Flex>
